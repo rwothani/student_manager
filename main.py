5b5c50ec2ef88967fa6f000a9ba979cbb68b4b47
rwothani <rwothania@gmail.com>
2026-02-14 14:05:39 +0300

    Add delete student functionality

--- main.py
+++ main.py
@@ -9,11 +9,31 @@ def view_students():
         for student in students:
             print(student.strip())
 
+def delete_student(name):
+    with open("students.txt", "r") as file:
+        students=file.readlines()
+
+    found=False
+
+    with open("students.txt", "w") as file:
+        for student in students:
+            if student.strip()!= name:
+                file.write(student)
+            else:
+                found=True
+
+    if found:
+        print(f"{name} deleted successfully.")
+    else:
+        print(f"{name}not found.")
+
+
 def menu():
     while True:
         print("\n1. Add Student")
         print("2. View students")
-        print("3. EXit")
+        print("3. Delete Student")
+        print("4. EXit")
         choice=input("Choose option: ")
 
         if choice=="1":
@@ -22,6 +42,9 @@ def menu():
         elif choice=="2":
             view_students()
         elif choice=="3":
+            name=input("Enter the name of the student to be deleted:n")
+            delete_student()
+        elif choice=="4":
             print("Goodbye!")
             break
         else:
